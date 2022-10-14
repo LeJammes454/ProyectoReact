450ba1a239e520509cbcb4288485ce3751d8397a
LeJammes454 <98853831+LeJammes454@users.noreply.github.com>
2022-10-14 14:22:02 -0500

    2 files modified, 2 added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import '@fontsource/roboto';
 
 import App from './App';
 import Car from './Car';
+import Vehiculo from './vehiculo/Vehiculo';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
@@ -15,6 +16,12 @@ root.render(
       imagen="https://i.pinimg.com/originals/a0/83/52/a08352bf98880fdad355ae5839bbda16.jpg"
       descripcion="Conoce la espectacular Camioneta Todoterreno Ford Ranger , la nueva
       integrante de la familia Ford"
+    />
+    <Vehiculo
+      imagen="https://www.mundomotero.com/wp-content/uploads/2020/10/Prueba-ZONTES-U1-125-2-1024x683.jpg"
+      nombre="Zontes U1"
+      descripcion="¿Una Trail ligera, dinámica y provocativa? Sin duda alguna, la respuesta es: U1-125."
+      modelo="u1 2022"
     />
   </StrictMode>
 );
